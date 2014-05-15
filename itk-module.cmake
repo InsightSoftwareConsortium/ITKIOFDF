1ff4166937374845e8eb2a158bd3511b81557ea4
Kent Williams <norman-k-williams@uiowa.edu>
2014-05-15 13:34:46 -0500

    COMP: Initial add for source

---
+++ itk-module.cmake
@@ -0,0 +1,13 @@
+set(DOCUMENTATION "This modules contains an ImageIO class to read or write the
+FDF image format.")
+
+itk_module(ITKIOFDF
+  DEPENDS
+    ITKNIFTI
+    ITKIOImageBase
+  TEST_DEPENDS
+    ITKTestKernel
+    ITKTransform
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
