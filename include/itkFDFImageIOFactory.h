d92ed1cceb7a4b393795998f34fda58a44fcbe28
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2015-02-26 08:57:02 +0100

    fix factory class name

--- include/itkFDFImageIOFactory.h
+++ include/itkFDFImageIOFactory.h
@@ -43,7 +43,7 @@ public:
   itkFactorylessNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(DicomImageIOFactory, ObjectFactoryBase);
+  itkTypeMacro(FDFImageIOFactory, ObjectFactoryBase);
 
   /** Register one factory of this type  */
   static void RegisterOneFactory(void)
