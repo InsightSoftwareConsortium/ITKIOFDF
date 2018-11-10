b14531aec23cf47305629e85f36563c397f2a330
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-11-10 15:47:16 -0500

    ENH: Add CI.
    Add CI for the module:
    - Add Circle, Travis and AppVeyor CI configuration `*.yml` files.
    - Add the `CTestConfig.cmake` file to submit builds to the **Inisght**
      project in the **open.cdash.org** dashboard.
    - Add the Python setup `setup.py` file.
    - Re-use the `README` file documentation in the `itk-module.cmake` file.

---
+++ setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from os import sys
+
+try:
+    from skbuild import setup
+except ImportError:
+    print('scikit-build is required to build from source.', file=sys.stderr)
+    print('Please run:', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('  python -m pip install scikit-build')
+    sys.exit(1)
+
+setup(
+    name='itk-fdfimageio',
+    version='1.0.0',
+    author='Glenn Pierce',
+    author_email='glennpierce@gmail.com',
+    packages=['itk'],
+    package_dir={'itk': 'itk'},
+    download_url=r'https://github.com/InsightSoftwareConsortium/ITKFDFImageIO',
+    description=r'ITK `ImageIO` class to read or write the FDF image format',
+    long_description='itkFDFImageIO provides an `ImageIO` class to read or'
+                     'write the FDF image format.',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: C++",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Operating System :: Android",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS"
+        ],
+    license='Apache',
+    keywords='ITK InsightToolkit FDF',
+    url=r'https://github.com/InsightSoftwareConsortium/ITKFDFImageIO',
+    install_requires=[
+        r'itk'
+    ]
+    )
