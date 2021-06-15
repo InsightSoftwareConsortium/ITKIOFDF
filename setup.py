02f7b9695eb2f7a1444c7d18eebcf4666a926325
Matt McCormick <matt.mccormick@kitware.com>
2021-06-15 15:33:35 -0400

    ENH: Update package for itk-5.2.0.post3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-iofdf',
-    version='1.0.1',
+    version='1.0.2',
     author='Glenn Pierce',
     author_email='glennpierce@gmail.com',
     packages=['itk'],
@@ -45,6 +45,6 @@ setup(
     keywords='ITK InsightToolkit FDF',
     url=r'https://github.com/InsightSoftwareConsortium/ITKIOFDF',
     install_requires=[
-        r'itk>=5.2rc3'
+        r'itk>=5.2.0.post3'
     ]
     )
